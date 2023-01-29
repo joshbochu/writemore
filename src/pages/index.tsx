aadd01bd0db20069fb8a9299966e3eb859e3b24f
josh <you@example.com>
2023-01-28 18:17:24 -0800

    add supabase helpers

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,9 @@
 export default function Home() {
   return (
     <div className="flex justify-center h-screen">
-      <textarea className="resize-none rounded w-1/3 h-full border-8 border-stone-200 outline-0">
+      <textarea placeholder="Write here..." className="resize-none rounded w-1/3 h-full border-8 border-stone-200 outline-0">
       </textarea>
+
     </div >
   )
 }
