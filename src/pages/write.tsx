65fbe5affb2ce83103ab2cb97271831e1c9eaf1a
josh <you@example.com>
2023-01-30 21:01:10 -0800

    make write component

---
+++ src/pages/write.tsx
@@ -0,0 +1,39 @@
+import { Auth, ThemeSupa } from '@supabase/auth-ui-react'
+import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react'
+import Account from '../components/Account'
+import { useState, useEffect } from 'react'
+
+const Write = () => {
+    const session = useSession()
+    const supabase = useSupabaseClient()
+    const [wordCount, setWordCount] = useState(0);
+    const [text, setText] = useState('');
+    const [darkMode, setDarkMode] = useState(false);
+    useEffect(() => {
+        setWordCount(text.split(/\s|\n/g).reduce((acc, curr) => curr ? acc + 1 : acc, 0));
+    }, [text]);
+
+    return (
+        <div className={`flex justify-center h-screen ${darkMode ? 'bg-gray-800' : 'bg-white'}`}>
+            <textarea
+                placeholder="Write here..."
+                className={`overflow-y-scroll custom-scrollbar rounded w-1/3 h-full outline-0 pt-16 ${darkMode ? 'bg-gray-800 text-white' : ''}`}
+                onChange={e => setText(e.target.value)}
+            >
+            </textarea>
+            <div className={`text-sm absolute bottom-0 right-0 p-4 ${darkMode ? 'text-white' : ''}`}>
+                {wordCount}
+            </div>
+            <div className="absolute top-0 right-0 p-4">
+                <img
+                    onClick={() => setDarkMode(!darkMode)}
+                    alt="toggle dark mode"
+                    src="https://upload.wikimedia.org/wikipedia/commons/a/a3/U%2B25D1.svg"
+                    style={{ filter: darkMode ? "invert(100%)" : "invert(0%)", width: "25px", height: "25px" }}>
+                </img>
+            </div>
+        </div>
+    );
+}
+
+export default Write
