dbc864b2453e6b99f7588f955182aa41267cc45e
joshbochu <joshuatbochu+git@gmail.com>
2023-02-06 22:43:53 -0800

    add title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { Auth, ThemeSupa } from '@supabase/auth-ui-react'
 import { useSession, useSupabaseClient, useUser } from '@supabase/auth-helpers-react'
 import Write from '../components/Write'
+import Head from 'next/head'
 
 const Home = () => {
   const session = useSession()
@@ -8,6 +9,9 @@ const Home = () => {
   const user = useUser()
   return (
     <>
+      <Head>
+        <title>WriteMore</title>
+      </Head>
       <Write session={session} supabase={supabase} user={user} />
     </>
   )
